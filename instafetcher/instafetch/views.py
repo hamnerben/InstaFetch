f28eeeac1d57dc665ad7b7d001f1ed6aa930e048
nathan <nathanctay@gmail.com>
2023-03-24 19:44:36 -0600

    Added views and urls. Created html files for login and signup

--- instafetcher/instafetch/views.py
+++ instafetcher/instafetch/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "instafetch/index.html")
+    return render(request, 'instafetch/index.html')
+
+def login(request):
+    return render(request, 'instafetch/login.html')
+
+def singup(request):
+    return render(request, 'instafetch/signup.html')
+
+def add(request):
+    pass
+
+def fetch(request):
+    pass
