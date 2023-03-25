82b5b09d664c6d9ec7d6d0fa34305810300d72cb
hamnerben <hamnerben@gmail.com>
2023-03-24 19:56:27 -0600

    added models

--- instafetcher/instafetch/models.py
+++ instafetcher/instafetch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
+class Page(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
