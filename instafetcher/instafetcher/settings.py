f94ba3865cda9c9d841b87c864deade39af2dc7a
nathan <nathanctay@gmail.com>
2023-03-24 19:08:35 -0600

    Added instafetch to installed apps

--- instafetcher/instafetcher/settings.py
+++ instafetcher/instafetcher/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instafetch.apps.IntafetchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
